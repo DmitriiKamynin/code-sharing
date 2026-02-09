5fa83b56edca2a5ee43e687207fdce797ba1a373
dmitriy.kamynin <dmitriy.kamynin@onetwotrip.com>
2026-02-09 16:34:12 +0700

    init opentelemetry

---
+++ packages/backend/src/opentelemetry.ts
@@ -0,0 +1,13 @@
+import { NodeSDK } from "@opentelemetry/sdk-node";
+import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+
+const sdk = new NodeSDK({
+    serviceName: "paircode",
+    traceExporter: new OTLPTraceExporter({
+        url: 'http://localhost:4318/v1/traces'
+    }),
+    instrumentations: [getNodeAutoInstrumentations()]
+})
+
+sdk.start();
